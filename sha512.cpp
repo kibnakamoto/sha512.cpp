9bf41d0a693d4982f9b88f290a7435fc82f2b690
kibnakamoto <86175813+kibnakamoto@users.noreply.github.com>
2021-12-04 09:45:40 -0500

    Update sha512.cpp

--- sha512.cpp
+++ sha512.cpp
@@ -1,42 +1,46 @@
 #include <iostream>
-#include <bitset>
 #include "constants.h"
 #include "operators.h"
-#include <string.h>
+#include <string>
 #include <vector>
 
-//  97, 115 = 24947 // convert string to binary then to decimal to use bitwise operations
+//  97, 115 = 24947 // convert string to binary then to decimal to use bitwise operators
 // a + s = 24947
 
 class SHA512
 {
     protected:
-        const unsigned int DIGEST_SIZE = 0x80; // 128
-        const unsigned int BLOCK_SIZE = 1024; // in bits
-        std::vector<std::bitset<8>> Word;
+        typedef unsigned char* ucharptr;
+        typedef unsigned long long uint64;
+        const uint64 DIGEST_SIZE = 0x80/8; // 128 bytes
+        const uint64 BLOCK_SIZE = 1024/8; // in bytes
+        std::vector<ucharptr> Word;
 
     public:
         SHA512(std::string msg)
         {
-            // length is represented by a 128 bit unsigned integer
-            __uint128_t len = msg.length()*8;
-
-
-            // padding of zeros
-            const __uint128_t padding = (BLOCK_SIZE - len+1 - 128) % 1024;
-            unsigned char* message;
+            ucharptr message = (ucharptr)msg.c_str();
             
-            for (int c=0;c<msg.length();c++)
+            // length is represented by a 128 bit unsigned integer
+            __uint128_t len = msg.length();
+
+            // padding
+            const __uint128_t padding = (BLOCK_SIZE - (len+1) - 128) % 1024;
+            ucharptr add1AndPad = message + '1' + '0'*padding + len;
+            Word.push_back(add1AndPad);
+
+            for (int i=0;i<Word.size();i++)
             {
-                message = (unsigned char*)msg.c_str();
+                std::cout << Word[i];
             }
         }
+        
 };
 
 int main()
 {
     std::string msg;
-    std::cout << "input message:\t";
+    std::cout << "input:\t";
     getline(std::cin, msg);
     SHA512 hash(msg);
     return 0;
