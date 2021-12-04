a4da8363a8459a1af84c7e9c089eb527684169ee
kibnakamoto <86175813+kibnakamoto@users.noreply.github.com>
2021-12-04 09:46:23 -0500

    Update operators.h

--- operators.h
+++ operators.h
@@ -1,1 +1,10 @@
+// choice
+#define Ch(x,y,z) (x bitand y)xor(~x bitand z)
 
+// majority
+#define Maj(x,y,z) (x & y)^(x & z)^(y & z)
+
+inline __uint128_t maj(__uint128_t x, __uint128_t y, __uint128_t z) 
+{
+    return (x bitand y)xor(x bitand z)xor(y bitand z);
+};
