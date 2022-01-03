66a087b2d58b7f2493811019ad3b5cd0540a44a8
kibnakamoto <86175813+kibnakamoto@users.noreply.github.com>
2022-01-03 17:05:01 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,9 +5,13 @@
 int main()
 {
     std::string msg;
-    msg = "";
+    msg = "abc";
     // std::cout << "input:\t";
     // getline(std::cin, msg);
     SHA512 hash(msg);
+    std::cout << std::endl
+          << "ddaf35a193617abacc417349ae20413112e6fa4e89a97ea20a9ee"
+          <<"ee64b55d39a2192992a274fc1a836ba3c23a3feebbd454d442364"
+          << "3ce80e2a9ac94fa54ca49f\n\t\t\t\t\t\t\t^ correct value ^";
     return 0;
 }
